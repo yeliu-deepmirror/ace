4217c47195d388cc43e68f97c407e42edc6309b7
yeliu <yeliu@deepmirror.com>
2024-07-19 11:19:05 +0800

    update

--- nano/nano_plus.py
+++ nano/nano_plus.py
@@ -3,104 +3,7 @@ import torch
 from torch import nn, Tensor
 from shufflenetv2 import build_shufflenetv2_backbone
 from ghost_pan import build_ghostpan_fpn
-
-
-  # aux_head_cfg:
-  #   type: SimpleConvHead
-  #   num_classes: 1
-  #   input_channel: 192
-  #   feat_channels: 192
-  #   stacked_convs: 8
-  #   strides: [8, 16, 32, 64]
-  #   act_cfg: LeakyReLU
-  #   reg_max: 7
-  #
-class SimpleConvHead(nn.Module):
-
-    def __init__(
-            self,
-            num_classes,
-            input_channel,
-            feat_channels=256,
-            stacked_convs=4,
-            strides=[8, 16, 32],  # noqa
-            conv_cfg=None,
-            norm_cfg=dict(type="GN", num_groups=32, requires_grad=True),  # noqa
-            act_cfg="LeakyReLU",  # noqa
-            reg_max=16,
-            **kwargs):
-        super(SimpleConvHead, self).__init__()
-        self.num_classes = num_classes
-        self.in_channels = input_channel
-        self.feat_channels = feat_channels
-        self.stacked_convs = stacked_convs
-        self.strides = strides
-        self.reg_max = reg_max
-
-        self.conv_cfg = conv_cfg
-        self.norm_cfg = norm_cfg
-        self.act_cfg = act_cfg
-        self.cls_out_channels = num_classes
-
-        self._init_layers()
-        self.init_weights()
-
-    def _init_layers(self):
-        self.relu = nn.ReLU(inplace=True)
-        self.cls_convs = nn.ModuleList()
-        self.reg_convs = nn.ModuleList()
-        for i in range(self.stacked_convs):
-            chn = self.in_channels if i == 0 else self.feat_channels
-            self.cls_convs.append(
-                ConvModule(
-                    chn,
-                    self.feat_channels,
-                    3,
-                    stride=1,
-                    padding=1,
-                    conv_cfg=self.conv_cfg,
-                    norm_cfg=self.norm_cfg,
-                    act_cfg=self.act_cfg,
-                ))
-            self.reg_convs.append(
-                ConvModule(
-                    chn,
-                    self.feat_channels,
-                    3,
-                    stride=1,
-                    padding=1,
-                    conv_cfg=self.conv_cfg,
-                    norm_cfg=self.norm_cfg,
-                    act_cfg=self.act_cfg,
-                ))
-        self.gfl_cls = nn.Conv2d(self.feat_channels, self.cls_out_channels, 3, padding=1)
-        self.gfl_reg = nn.Conv2d(self.feat_channels, 4 * (self.reg_max + 1), 3, padding=1)
-        self.scales = nn.ModuleList([Scale(1.0) for _ in self.strides])
-
-    def init_weights(self):
-        for m in self.cls_convs:
-            normal_init(m.conv, std=0.01)
-        for m in self.reg_convs:
-            normal_init(m.conv, std=0.01)
-        bias_cls = -4.595
-        normal_init(self.gfl_cls, std=0.01, bias=bias_cls)
-        normal_init(self.gfl_reg, std=0.01)
-
-    def forward(self, feats):
-        outputs = []
-        for x, scale in zip(feats, self.scales):
-            cls_feat = x
-            reg_feat = x
-            for cls_conv in self.cls_convs:
-                cls_feat = cls_conv(cls_feat)
-            for reg_conv in self.reg_convs:
-                reg_feat = reg_conv(reg_feat)
-            cls_score = self.gfl_cls(cls_feat)
-            bbox_pred = scale(self.gfl_reg(reg_feat)).float()
-            output = torch.cat([cls_score, bbox_pred], dim=1)
-            outputs.append(output.flatten(start_dim=2))
-        outputs = torch.cat(outputs, dim=2).permute(0, 2, 1)
-        return outputs
+from nanodet_plus_head import build_head
 
 
 class NanoLines(nn.Module):
@@ -120,14 +23,58 @@ class NanoLines(nn.Module):
 
         self.backbone = build_shufflenetv2_backbone(nano_config["backbone_cfg"])
         self.fpn = build_ghostpan_fpn(nano_config["fpn_cfg"])
-        self.head = build_nanodet_plus_head(nano_config["head_cfg"])
-        self.detach_epoch = detach_epoch
+        self.head = build_head(nano_config["head_cfg"])
 
 
     def forward(self, x: torch.Tensor) -> Tensor:
-        im_shape = x.shape[2:]  # HxW
         bb_feats = self.backbone(x)
         fpn_feats = self.fpn(bb_feats)
-        # head_out = self.head(feats=fpn_feats, im_shape=im_shape)
-
+        # print(len(fpn_feats))
+        head_out = self.head(feats=fpn_feats)
         return head_out
+
+
+
+def save_model(model, path, epoch, iter, optimizer=None):
+    print("save model to", path)
+    model_state_dict = (
+        model.module.state_dict() if hasattr(model, "module") else model.state_dict()
+    )
+    data = {"epoch": epoch, "state_dict": model_state_dict, "iter": iter}
+    if optimizer is not None:
+        data["optimizer"] = optimizer.state_dict()
+    torch.save(data, path)
+
+
+
+def load_model_weight(model, model_path):
+    print("load model from", model_path)
+    checkpoint = torch.load(model_path)
+    state_dict = checkpoint["state_dict"].copy()
+    for k in checkpoint["state_dict"]:
+        # convert average model weights
+        if k.startswith("avg_model."):
+            v = state_dict.pop(k)
+            state_dict[k[4:]] = v
+    # strip prefix of state_dict
+    if list(state_dict.keys())[0].startswith("module."):
+        state_dict = {k[7:]: v for k, v in state_dict.items()}
+    if list(state_dict.keys())[0].startswith("model."):
+        state_dict = {k[6:]: v for k, v in state_dict.items()}
+
+    model_state_dict = (
+        model.module.state_dict() if hasattr(model, "module") else model.state_dict()
+    )
+
+    # check loaded parameters and created model parameters
+    for k in state_dict:
+        if k in model_state_dict:
+            if state_dict[k].shape != model_state_dict[k].shape:
+                state_dict[k] = model_state_dict[k]
+        else:
+            logger.log("Drop parameter {}.".format(k))
+    for k in model_state_dict:
+        if not (k in state_dict):
+            print("No param", k)
+            state_dict[k] = model_state_dict[k]
+    model.load_state_dict(state_dict, strict=False)
--- nano/train.py
+++ nano/train.py
@@ -1,6 +1,7 @@
 import torch
 import glob
 import yaml
+import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 
@@ -10,33 +11,50 @@ from torch.utils.data import Dataset, DataLoader
 from torch.utils.data import SubsetRandomSampler
 import torchvision.transforms as transforms
 
-from nano_plus import NanoLines
+from nano_plus import NanoLines, save_model, load_model_weight
 from dataset.dataset import CarLinemarksDataset
 
 config_file = "nano/config/gray_config.yaml"
 data_set_folder = "data/20240222T101812+0800_oppoma_/dataset"
+model_path = "models/model_nano_lines.ckpt"
+max_num_epoches = 1000
 
-
-def get_optimizer(opt_cfg):
+def get_optimizer(model, opt_cfg):
     cfg_args = opt_cfg.copy()
     func_args = {}
     func_args.update(cfg_args)
-    return torch.optim.AdamW(**func_args)
+    return torch.optim.AdamW([
+        {"params": model.head.parameters()},
+        {"params": model.fpn.parameters()},
+    ], **func_args)
 
 
-def get_scheduler(scheduler_cfg):
+def get_scheduler(optimizer, scheduler_cfg):
     cfg_args = scheduler_cfg.copy()
     func_args = {}
     func_args.update(cfg_args)
-    return torch.optim.lr_scheduler.CosineAnnealingLR(**func_args)
+    return torch.optim.lr_scheduler.CosineAnnealingLR(optimizer=optimizer, **func_args)
 
 
 def label_loss(output, target):
     # first element indicate if the line exist
     # other elements are the line position
     # TODO: make a better loss
-    loss_positions = target[::,1:]
+    diff_positions = torch.norm(output[:,:,1:] - target[:,:,1:], dim=2)
+    loss_positions = torch.norm(torch.mul(diff_positions, target[:,:,0]), dim=1)
+    label_weight = 100;
+    loss_labels = torch.norm(output[:,:,0] - target[:,:,0], dim=1)
+    loss = loss_positions + label_weight * loss_labels
+    return loss.sum()
 
+
+def run_test(det_model, test_loader):
+    test_loss = 0
+    # det_model.cpu()
+    for i, data in enumerate(test_loader, 0):
+        outputs = det_model(data[0].cuda())
+        test_loss += label_loss(outputs.cpu(), data[1]).item()
+    return test_loss
 
 
 with open(config_file) as stream:
@@ -49,6 +67,7 @@ dataset = CarLinemarksDataset(data_set_folder)
 # splitting training and testing sets
 batch_size = 32
 indices = list(range(len(dataset)))
+random.seed(15612)
 random.shuffle(indices)
 split_point = int(0.85*len(indices))
 train_indices = indices[:split_point]
@@ -59,33 +78,37 @@ print("Size of the  testing set:", len(test_indices))
 train_loader = DataLoader(dataset, batch_size=batch_size, sampler=SubsetRandomSampler(train_indices))
 test_loader = DataLoader(dataset, batch_size=batch_size, sampler=SubsetRandomSampler(test_indices))
 
+det_model = NanoLines(nano_config).cuda()
+
+# load the network
+# det_model.load_state_dict(torch.load(model_path))
+load_model_weight(det_model, model_path)
+
+optimizer = get_optimizer(det_model, nano_config["optimizer_cfg"])
+scheduler = get_scheduler(optimizer, nano_config["lr_scheduler_cfg"])
 
 
-det_model = NanoLines(nano_config)
+for epoch in range(max_num_epoches):
+    det_model.cuda()
+    print("run epoch", epoch)
+    train_loss_sum = 0
+    for i, data in enumerate(test_loader, 0):
+        # Clear gradients
+        optimizer.zero_grad()
+        outputs = det_model(data[0].cuda())
+        loss = label_loss(outputs.cpu(), data[1])
+        loss.backward()
+        optimizer.step()
 
-# test = det_model(dataset[0][0])
-# print(test)
+        train_loss_sum += loss.item()
+    torch.cuda.empty_cache()
 
-# print(det_model)
-
-for i, data in enumerate(train_loader, 0):
-    print(i)
-    print(data[0].shape)
-    print(data[1].shape)
-    test = det_model(data[0])
-    print(test)
-    break
-
-
-        # self.optimizer = self.context.wrap_optimizer(
-        #     build_optimizer(self.cfg.optimizer_cfg,
-        #                     params=filter(lambda p: p.requires_grad, self.model.parameters())))
-        # self.optimizer.zero_grad()
-        # lr_scheduler = build_lr_scheduler(self.cfg.lr_scheduler_cfg, optimizer=self.optimizer)
-        # self.scheduler = self.context.wrap_lr_scheduler(
-        #     lr_scheduler,
-        #     step_mode=LRScheduler.StepMode.STEP_EVERY_EPOCH,
-        #     frequency=self.get_lr_scheduler_frequency())
-
-# optimizer = get_optimizer(nano_config["optimizer_cfg"]);
-# scheduler = get_scheduler(nano_config["lr_scheduler_cfg"]);
+    scheduler.step()
+    if epoch%10 == 0:
+        with torch.no_grad():
+            test_loss_sum = run_test(det_model, test_loader)
+            print(" => test_loss_sum :", test_loss_sum)
+    if epoch%50 == 0:
+        # torch.save(det_model.state_dict(), model_path)
+        save_model(det_model, model_path, epoch, 0)
+    print(" => train_loss_sum :", train_loss_sum)
